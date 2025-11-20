0e14706de49ce3baa7266be82c881fc3d70b2cc2
Vladimir <nikonoshin1999@gmail.com>
2025-11-21 00:48:17 +0400

    Создали новые классы и методы позволяющие добавлять и обновлять пользователей и фильмы

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+
+	private static final LocalDate MIN_DATE = LocalDate.of(1895, 12, 28);
+	private Long id;
+	private String name;
+	private String description;
+	private LocalDate releaseDate;
+	private long duration;
+
+	public static LocalDate getMinDate() {
+		return MIN_DATE;
+	}
 }
+
+
